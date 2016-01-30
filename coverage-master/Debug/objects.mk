0fed3bef5d74fcca9a7ba64e1b7bb48c5148fbea
Ben Lengerich <blengerich@gmail.com>
2016-01-29 19:23:03 -0500

    foobar

--- coverage-master/Debug/objects.mk
+++ coverage-master/Debug/objects.mk
@@ -2,6 +2,6 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := /home/ben/temp/googletest-master/googletest/make/gtest.a
+USER_OBJS := /usr/local/lib/gtest.a
 
 LIBS := -lgtest -lgcov -lpthread -lgtest_main
