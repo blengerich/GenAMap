9741570549577bdab893b589fe5d88e8be9d6120
blengerich <bjl5226@psu.edu>
2016-01-30 15:25:02 -0500

    added testing folder

---
+++ testing/sqrt_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <math.h>
+
+float square-root(float n) {
+    return math::sqrt(n);
+}
+
+TEST (SquareRootTest, PositiveNos) {
+        EXPECT_EQ (18.0, square-root (324.0));
+            EXPECT_EQ (25.4, square-root (645.16));
+                EXPECT_EQ (50.3321, square-root (2533.310224));
+}
+
+TEST (SquareRootTest, ZeroAndNegativeNos) {
+        ASSERT_EQ (0.0, square-root (0.0));
+            ASSERT_EQ (-1, square-root (-22.0));
+}
