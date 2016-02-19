37d226bbf5a2942de6e352149e5be96f19b7bcba
Ben Lengerich <blengerich@gmail.com>
2016-02-18 22:36:28 -0500

    fixed test_all script

--- src/test_all.sh
+++ src/test_all.sh
@@ -3,7 +3,7 @@ tests=(//Scheduler:Scheduler_Tests //examples:my_test)
 result=0
 bazel=../depends/bazel_install/binary/bazel
 for test in $tests; do
-	OUTPUT="$(bazel test ${test} --test_output=errors)"
+	OUTPUT="$(${bazel} test ${test} --test_output=errors)"
 	cur_result=$?
 	echo $OUTPUT
 	if [ $? -ne 0 ]
