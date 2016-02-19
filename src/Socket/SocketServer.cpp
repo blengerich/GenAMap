efa88e679db76ce0bccf1ada23ea1c9a47184b83
WangHaohan <haohanwang@wanghaohans-air.wv.cc.cmu.edu>
2016-02-19 18:27:21 -0500

    socket methods

---
+++ src/Socket/SocketServer.cpp
@@ -0,0 +1,66 @@
+//
+// Created by WangHaohan on 2/19/16.
+//
+
+#include "SocketServer.h"
+
+SocketServer::SocketServer(int protno) { this->portno = protno; }
+
+int SocketServer::serve(int times) {
+    pthread_t thread;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        throw std::runtime_error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0) {
+        throw std::runtime_error("ERROR on binding");
+    }
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+    int i = 0;
+    while (i < times) {
+        /* accept incoming connections */
+        newsockfd = accept(sockfd,
+                           (struct sockaddr *) &cli_addr,
+                           &clilen);
+        if (newsockfd < 0)
+            throw std::runtime_error("ERROR on accept");
+        else {
+            /* start a new thread but do not wait for it */
+            cout << "This is process # " << to_string(i+1) << endl;
+            pthread_create(&thread, 0, SocketServer::process, &newsockfd);
+            pthread_detach(thread);
+        }
+        i++;
+    }
+    close(sockfd);
+}
+
+void* SocketServer::process(void *ptr) {
+    int len;
+    int *conn;
+    long addr = 0;
+    char buffer[65535];
+    if (!ptr) pthread_exit(0);
+
+    conn = (int*) ptr;
+    bzero(buffer, 65535);
+    int flag;
+    flag = read(*conn, buffer, 65535);
+    if (flag < 0) throw std::runtime_error("ERROR reading from socket");
+    string str(buffer);
+    mainMessage mm = JsonCoder::getInstance().decodeMainMessage(str);
+    string m = JsonCoder::getInstance().encodeMainMessage(mm);
+    const char *r = m.c_str();
+    flag = write(*conn, r, 65535);
+    if (flag < 0) throw std::runtime_error("ERROR writing to socket");
+    close(*conn);
+
+    pthread_exit(0);
+}
+
---
+++ src/Socket/SocketServer.h
@@ -0,0 +1,45 @@
+//
+// Created by WangHaohan on 2/19/16.
+//
+
+#ifndef GENAMAP_V2_SOCKETSERVER_H
+#define GENAMAP_V2_SOCKETSERVER_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdexcept>
+#include <iostream>
+#include <algorithm>
+#include <pthread.h>
+#include <vector>s
+
+#include "json/JsonCoder.hpp"
+
+using namespace std;
+
+
+class SocketServer {
+
+private:
+    int sockfd, newsockfd; //file descriptors
+    int portno; //port number on which the server accepts connections
+    socklen_t clilen; //size of the address of the client
+//    char buffer[65535]; //server reads characters from the socket connection into this buffer.
+    struct sockaddr_in serv_addr, cli_addr; //structures containing an internet address
+    int flag;
+
+public:
+    SocketServer(int);
+    int serve(int);
+
+    static void *process(void *ptr);
+
+};
+
+#endif //GENAMAP_V2_SOCKETSERVER_H
