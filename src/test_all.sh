c016af4352da7ef22ab9f2ed18ffa35765e1d4ab
Ben Lengerich <blengerich@gmail.com>
2016-04-22 19:06:44 -0400

    fixed test script

--- src/test_all.sh
+++ src/test_all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-tests=(//Scheduler:Scheduler_Tests //examples:my_test)
+tests=(//Scheduler:Scheduler_Tests)
 result=0
 bazel=../depends/bazel_install/binary/bazel
+flags="--spawn_strategy=standalone --test_output=errors --verbose_failures"
 for test in "${tests[@]}"; do
-	OUTPUT="$(${bazel} test $test --test_output=errors)"
+	OUTPUT="$(${bazel} test $test ${flags})"
 	cur_result=$?
 	echo $OUTPUT
 	if [ $? -ne 0 ]
