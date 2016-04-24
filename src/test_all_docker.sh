f65ced54dfeca499eab1f89f0be319f7d41f484c
Ben Lengerich <blengerich@gmail.com>
2016-04-24 02:11:10 -0400

    exit status

--- src/test_all_docker.sh
+++ src/test_all_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run -i -v ${PWD}/../:/usr/src/genamap blengerich/genamap /bin/bash -c "cd /usr/src/genamap/src; ./test_all.sh"
+sudo docker run -i -v ${PWD}/../:/usr/src/genamap blengerich/genamap /bin/bash -c "cd /usr/src/genamap/src; ./test_all.sh; exit $?"
 exit $?
