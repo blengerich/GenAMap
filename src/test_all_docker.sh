77d3e18f7036e8164dfaed5ffdd4f1065ae38db2
Ben Lengerich <blengerich@gmail.com>
2016-04-24 02:07:57 -0400

    exit code corrected

--- src/test_all_docker.sh
+++ src/test_all_docker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker run -i -v ${PWD}/../:/usr/src/genamap blengerich/genamap /bin/bash -c "cd /usr/src/genamap/depends; chmod +x install_bazel.sh; ./install_bazel.sh; cd ../src; ./test_all.sh"
+sudo docker run -i -v ${PWD}/../:/usr/src/genamap blengerich/genamap /bin/bash -c "cd /usr/src/genamap/src; ./test_all.sh"
+exit $?
