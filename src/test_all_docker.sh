af57c9f2420c6aa1385c4a40a99542e8c4c38741
Ben Lengerich <blengerich@gmail.com>
2016-04-24 02:03:37 -0400

    trying new docker runner file

--- src/test_all_docker.sh
+++ src/test_all_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -i -p 8000:3000 -v ${PWD}/../:/usr/src/genamap blengerich/genamap /bin/bash -c "cd /usr/src/genamap/src; ./test_all.sh"
+sudo docker run -i -v ${PWD}/../:/usr/src/genamap blengerich/genamap /bin/bash -c "cd /usr/src/genamap/depends; chmod +x install_bazel.sh; ./install_bazel.sh; cd ../src; ./test_all.sh"
