2d4e358e9299953a5d256ca5704c4a819be9c3a3
Ben Lengerich <blengerich@gmail.com>
2016-05-11 15:46:15 -0400

    output test errors

--- src/scripts/test_all.sh
+++ src/scripts/test_all.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
-
+set -e
 # Should be run from INSIDE a Docker container
 tests=(//Scheduler:Scheduler_Tests)
-result=0
-bazel=../../depends/bazel_install/binary/bazel
-flags="--spawn_strategy=standalone --test_output=errors --verbose_failures"
+bazel=/usr/src/genamap/depends/bazel_install/binary/bazel
+flags="--spawn_strategy=standalone --test_output=errors --color=yes --ignore_unsupported_sandboxing"
 for test in "${tests[@]}"; do
-	OUTPUT="$(${bazel} test $test ${flags})"
-	cur_result=$?
-	echo $OUTPUT
-	if [ $? -ne 0 ]
-		then result=1
-	fi
+	cmd="${bazel} test $test ${flags}"
+	eval "$cmd 2>&1"
 done
 cd /usr/src/genamap/src/Scheduler/node
 node-gyp rebuild
-if [ $? -ne 0 ]
-	then result=1
-fi
-
-exit ${result}
