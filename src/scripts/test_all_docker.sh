d8da59e71ae2cf64500b4a3d4ba6f62cadd7747a
Ben Lengerich <blengerich@gmail.com>
2016-05-11 16:51:32 -0400

    clarifying test script functions

--- src/scripts/test_all_docker.sh
+++ src/scripts/test_all_docker.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-sudo docker run -i -v ${PWD}/../../:/usr/src/genamap blengerich/genamap /bin/bash -c "cd /usr/src/genamap/depends; ./install_bazel.sh; cd /usr/src/genamap/src/scripts; ./test_all.sh; exit $?"
-exit $?
+set -e
+# Should be run from INSIDE a Docker container
+rebuild_node=1
+
+tests=(//Scheduler:Scheduler_Tests)
+bazel=/usr/src/genamap/depends/bazel_install/binary/bazel
+flags="--spawn_strategy=standalone --test_output=all --color=yes --ignore_unsupported_sandboxing"
+for test in "${tests[@]}"; do
+	cmd="${bazel} test $test ${flags}"
+	eval "$cmd 2>&1"
+done
+
+if [ $rebuild_node -eq 1 ]
+	then cd /usr/src/genamap/src/Scheduler/node
+	node-gyp rebuild
+fi
