70226b594ccd1a681a04f3603846b24a0629180f
Ben Lengerich <blengerich@gmail.com>
2016-05-25 20:37:20 -0400

    add model tests to test script

--- src/scripts/test_all_docker.sh
+++ src/scripts/test_all_docker.sh
@@ -6,7 +6,7 @@ rebuild_node=1
 node_tests=1
 
 if [ $cpp_tests -eq 1 ]
-	then tests=(//Stats:Stats_Tests //Scheduler:Scheduler_Tests)
+	then tests=(//model:Model_Tests //Stats:Stats_Tests //Scheduler:Scheduler_Tests)
 	flags="--test_verbose_timeout_warnings --spawn_strategy=standalone --test_output=all --color=yes --ignore_unsupported_sandboxing"
 	for test in "${tests[@]}"; do
 		cmd="bazel test $test ${flags}"
