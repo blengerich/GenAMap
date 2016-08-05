b7fd01799451550ab7656be5517bf3fa308d76ef
Beilin Li <nix7bg2mj1@gmail.com>
2016-08-05 12:34:24 -0400

    new design

--- src/frontend/src/components/GMLogin.js
+++ src/frontend/src/components/GMLogin.js
@@ -1,11 +1,42 @@
 import React, { Component, PropTypes } from 'react'
+import AppBar from 'material-ui/lib/app-bar'
+import RaisedButton from 'material-ui/lib/raised-button'
+import TextField from 'material-ui/lib/text-field'
+import Dialog from 'material-ui/lib/dialog'
+
+import config from '../../config'
+
+const styles = {
+  action: {
+    margin: '5% 5% 5% 0'
+  },
+  appBar: {
+    backgroundColor: config.ui.baseColor,
+    marginBottom: '5%'
+  },
+  background: {
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  },
+  body: {
+    backgroundColor: '#FFFFFF',
+    fontFamily: 'Roboto, sans-serif',
+    position: 'absolute',
+    left: '35%',
+    top: '20%',
+    width: '30%',
+    height: '50%',
+    textAlign: 'center'
+  }
+}
 
 class Login extends Component {
-
   render () {
     const { errorMessage } = this.props
 
-    return (
+    const oldView = (
       <div>
         <input
           type={'text'}
@@ -27,6 +58,42 @@ class Login extends Component {
         </button>
 
         {errorMessage && <p>{errorMessage}</p>}
+      </div>
+    )
+
+    return (
+      <div style={styles.background}>
+        <div style={styles.body}>
+          <AppBar
+            title='GenAMap 2.0'
+            style={styles.appBar}
+            showMenuIconButton={false}
+          />
+          <p style={{ 'fontSize': '1.8em' }}>Login to GenAMap</p>
+          <div>
+            <TextField
+              hintText='Username'
+            /><br/>
+          </div>
+          <div>
+            <TextField
+              hintText='Password'
+              type='password'
+            /><br/>
+          </div>
+          <div>
+            <RaisedButton
+              label='Create Account'
+              secondary={true}
+              style={styles.action}
+            />
+            <RaisedButton
+              label='Login'
+              primary={true}
+              styles={styles.action}
+            />
+          </div>
+        </div>
       </div>
     )
   }
