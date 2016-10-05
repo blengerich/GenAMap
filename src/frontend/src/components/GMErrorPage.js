6968fdf82b1381848b4978b16972f8e3a0137afb
daniellehu <daniellh@andrew.cmu.edu>
2016-10-05 15:32:01 -0400

    added new style to error password change page

--- src/frontend/src/components/GMErrorPage.js
+++ src/frontend/src/components/GMErrorPage.js
@@ -4,44 +4,12 @@ import fetch from './fetch'
 import config from '../../config'
 
 const styles = {
-  action: {
-    margin: '10px 0',
-    width: '70%'
-  },
-  appBar: {
-    backgroundColor: config.ui.baseColor,
-    marginBottom: '5%'
-  },
-  background: {
-    backgroundColor: 'rgba(0, 0, 0, 0.25)',
-    position: 'absolute',
-    width: '100%',
-    height: '100%'
-  },
-  body: {
-    backgroundColor: '#FFFFFF',
-    fontFamily: 'Roboto, sans-serif',
-    position: 'absolute',
-    left: '35%',
-    top: '20%',
-    width: '30%',
-    height: '60%',
-    textAlign: 'center'
-  },
-  form: {
-    width: '70%'
-  },
-  header: {
-    margin: '0 0 0 10px',
-    paddingTop: 0,
-    fontFamily: 'Roboto, sans-serif',
-    fontSize: '2.2em',
-    fontWeight: 400,
-    color: '#ffffff',
-    lineHeight: '64px'
+  textStyle: {
+    fontFamily: Raleway,
+    marginTop: '100px',
+    textAlign: center
   }
 }
-
 
 const GMErrorPage = React.createClass({
   componentDidMount() {
@@ -63,11 +31,15 @@ const GMErrorPage = React.createClass({
   },
   render() {
     return (
-      <div style={styles.background}>
-      OOOPS!
-        <br/>
-          {this.props.params.name}
-      </div>
+      <head>
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"></link>
+      </head>
+      <body style={styles.textStyle}>
+          <h1>Error!</h1>
+          <img style="width: 300px; height: 300px" src="../images/sad-face.svg"/>
+          <p>Sorry, but your input was invalid.</p>
+          <p>Check: {this.props.params.name}</p>
+      </body>
     )
   }
 })
