40fa208dcc241bc247d37d5b9a6c5368f89b8a18
daniellehu <daniellh@andrew.cmu.edu>
2016-11-30 02:03:44 -0500

    implementing the cookie system for new users

--- src/frontend/src/components/AuthenticatedComponent.js
+++ src/frontend/src/components/AuthenticatedComponent.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { redirectToLogin } from '../actions'
 
+const welcomeSrc = 'images/welcome.svg';
+
 export function requireAuthentication (Component) {
   class AuthenticatedComponent extends React.Component {
 
@@ -17,13 +19,34 @@ export function requireAuthentication (Component) {
       if (!this.props.isAuthenticated) {
         const redirectAfterLogin = this.props.location.pathname
         this.props.redirectToLogin(redirectAfterLogin)
+      } else {
+        if (document.cookie.indexOf("visited") >= 0) {
+          // already visited before, do not show welcome screen
+          setTimeout(function() {
+            document.getElementById("welcomeScreen").remove()
+          }, 100)
+        } else {
+          // first time visiting, show welcome screen
+          document.cookie = "visited"; 
+          setTimeout(function() {
+            document.getElementById("welcomeScreen").style.visibility = "visible"
+          }, 100)
+        }
       }
     }
 
     render () {
+
       return (
         <div>
-          {this.props.isAuthenticated && <Component {...this.props} />}
+          <div id="welcomeScreen" style={{"position": "fixed", "visibility": "hidden", "top": "0%", 
+          "left": "0%", "height": "100%", "width": "100%", "textAlign": "center", 
+          "zIndex":"99999999999999999999999", "backgroundColor": "white"}}>
+            <img src={welcomeSrc}/>
+          </div>
+          <div>
+            {this.props.isAuthenticated && <Component {...this.props} />}
+          </div>
         </div>
       )
     }
