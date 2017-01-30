bc131491972cdd3f9150753ebbb23f5f9deaf422
Ben Lengerich <blengerich@gmail.com>
2017-01-29 20:46:26 -0500

    update launch script, should only need to run ./genamap.sh run to launch GenAMap

--- scripts/genamap.sh
+++ scripts/genamap.sh
@@ -9,10 +9,22 @@
 SCRIPT=genamap
 
 #Initialize variables to default values.
-OPT_PORT=49160
+OPT_PORT=7000
 OPT_IMAGE_TAG=latest
-OPT_NAME=chasm
-IP=`docker-machine ip default`
+OPT_NAME=genamap
+
+# Discover platform and set default IP depending on it.
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  IP=localhost
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Mac OSX
+  IP=`docker-machine ip default`  # for Mac  
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  # POSIX compatibility layer and Linux environment emulation for Windows
+  IP="" # I don't know the Docker IP for Windows
+else
+  'echo "Running on unrecognized platform" '
+fi
 
 #Set fonts for Help.
 NORM=`tput sgr0`
@@ -23,9 +35,10 @@ REV=`tput smso`
 function HELP {
   echo -e \\n"Help documentation for ${BOLD}${SCRIPT}.${NORM}"\\n
   echo -e "${REV}Basic usage:${NORM} ${BOLD}${SCRIPT} [command]${NORM}"\\n
-  echo -e "[command] can be one of the following: node, gulp, docker (default is node if no command is passed in). gulp can only be run if a container is already running (most likely from running genamap node). docker will start the docker daemon, which needs to be running for node and gulp to work."\\n
-  echo -e "run will forward the port and load your current directory (used to start nodemon)"\\n
-  echo -e "exec will not forward the port nor load your current directory (used to run gulp)"\\n
+  echo -e "[command] can be one of the following: run, gulp, docker."\\n
+  echo -e "${BOLD}docker${NORM} will start the docker daemon, which needs to be running for node and gulp to work on Mac (and Windows?)."\\n
+  echo -e "${BOLD}run${NORM} will start a docker container, rerun node-gyp if the module does not exist, and launch the node app in a new browser tap."\\n
+  echo -e "${BOLD}gulp${NORM} will start gulp (which should not be expected to return). ${BOLD}gulp${NORM} can only be run if a container is already running (most likely from running genamap run)."\\n
   echo "Command line switches are optional. The following switches are recognized."
   echo "${REV}-p${NORM}  --Sets the value for the ${BOLD}port docker forwards to${NORM}. Default is ${BOLD}${OPT_PORT}${NORM}.Only used with node command."
   echo "${REV}-t${NORM}  --Sets the value for the ${BOLD}tag of the image${NORM}. Default is ${BOLD}${OPT_IMAGE_TAG}${NORM}. Only used with node command."
@@ -38,23 +51,19 @@ function DOCKER {
     bash --login '/Applications/Docker/Docker Quickstart Terminal.app/Contents/Resources/Scripts/start.sh'
 }
 
-function RUN {
-  docker run --name ${OPT_NAME} -ti --rm -p ${OPT_PORT}:3000 -v ${PWD}:/usr/src/genamap blengerich/genamap:${OPT_IMAGE_TAG}
-}
-
 function NODEGYP {
-  docker run --name ${OPT_NAME} -ti --rm -p ${OPT_PORT}:3000 -v ${PWD}:/usr/src/genamap blengerich/genamap:${OPT_IMAGE_TAG} /bin/bash -c "cd /usr/src/genamap/Scheduler/node && node-gyp rebuild"
+  docker run --name ${OPT_NAME} -ti --rm -p ${OPT_PORT}:3000 -v ${PWD}/../src:/usr/src/genamap blengerich/genamap:${OPT_IMAGE_TAG} /bin/bash -c "cd /usr/src/genamap/Scheduler/node && node-gyp rebuild"
 }
 
-function NODE {
-  if [ ! -f ./src/Scheduler/node/build/Release/scheduler.node ]; then
+function RUN {
+  if [ ! -f ../src/Scheduler/node/build/Release/scheduler.node ]; then
     NODEGYP
   fi
-  open "http://${IP}:${OPT_PORT}" && docker run --name ${OPT_NAME} -ti --rm -p ${OPT_PORT}:3000 -v ${PWD}:/usr/src/genamap blengerich/genamap:${OPT_IMAGE_TAG} /bin/bash -c "cd /usr/src/genamap/frontend/genamapApp && nodemon -L webapp.js"
+  xdg-open "http://${IP}:${OPT_PORT}" && docker run --name ${OPT_NAME} -ti --rm -p ${OPT_PORT}:3000 -v ${PWD}/../src:/usr/src/genamap blengerich/genamap:${OPT_IMAGE_TAG} /bin/bash -c "cd /usr/src/genamap/frontend/ && nodemon -L webapp.js"
 }
 
 function GULP {
-  docker exec ${OPT_NAME} /bin/bash -c "cd /usr/src/genamap/src/frontend/genamapApp; gulp watch" && docker attach ${OPT_NAME}
+  docker exec ${OPT_NAME} /bin/bash -c "cd /usr/src/genamap/frontend; gulp watch" && docker attach ${OPT_NAME}
 }
 
 ### Start getopts code ###
@@ -77,6 +86,7 @@ while getopts :p:t:n:h FLAG; do
       ;;
     h)  #show help
       HELP
+      exit 0
       ;;
     \?) #unrecognized option - show help
       echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
@@ -90,17 +100,15 @@ while getopts :p:t:n:h FLAG; do
 done
 
 shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
-
 ### End getopts code ###
 
 #Check the number of arguments. If none are passed, print help and exit.
 NUMARGS=$#
 if [ $NUMARGS -eq 0 ]; then
-  NODE
+  HELP
 fi
 
 ### Main loop to process command ###
-
 while [ $# -ne 0 ]; do
   COMMAND=$1
   if [ "$COMMAND" == "docker" ]; then
@@ -112,9 +120,6 @@ while [ $# -ne 0 ]; do
   elif [ "$COMMAND" == "nodegyp" ]; then
     NODEGYP
     exit 1
-  elif [ "$COMMAND" == "node" ]; then
-    NODE
-    exit 1
   elif [ "$COMMAND" == "gulp" ]; then
     GULP
     exit 1
@@ -125,6 +130,6 @@ while [ $# -ne 0 ]; do
   shift  #Move on to next command
 done
 
-### End main loop ###
 
+### End main loop ###
 exit 0
