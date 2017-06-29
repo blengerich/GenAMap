8258ed91638e8ceea7a97be751f33ccc45634fab
Jeremy Ong <tjong1998@gmial.com>
2017-06-29 17:02:11 -0400

    edit development files
    gulpfile.js has a mybuild task which allows for bundling of css
    package.json supports needed dependencies
    start_mongo.sh stores the mongodatabase in a local dbpath folder

--- src/frontend/gulpfile.js
+++ src/frontend/gulpfile.js
@@ -3,10 +3,74 @@ var browserify = require('browserify')
 var source = require('vinyl-source-stream')
 var watchify = require('watchify')
 
+var appBundler = browserify({
+  entries: ['src/index.js'],
+  transform: [
+    ['babelify', {
+      "presets": ['es2015', 'react'],
+      "plugins": ['transform-object-rest-spread'],
+      "compact" : false
+      }
+    ],
+    ['browserify-css']
+  ]
+}, { debug : true });
+
+gulp.task('mybundle', function () {
+  return appBundler
+    .bundle()
+    .on('error', function (err) {
+      console.error(err.message)
+      console.error(err.codeFrame)
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('static/'))
+})
+
+gulp.task('mywatch', function () {
+  var b = browserify({
+    entries: ['src/index.js'],
+    transform: [
+      ['babelify', {
+        "presets": ['es2015', 'react'],
+        "plugins": ['transform-object-rest-spread'],
+        "compact" : false
+        }
+      ],
+      ['browserify-css']
+    ],
+    cache: {},
+    packageCache: {},
+    plugin: [watchify]
+  }, { debug : true });
+
+  b.on('update', makeBundle)
+
+  function makeBundle () {
+      b
+      .bundle()
+      .on('error', function (err) {
+        console.error(err.message)
+        console.error(err.codeFrame)
+      })
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('static/'))
+    console.log('Bundle updated, success')
+  }
+
+  makeBundle()
+
+  return b
+})
+
 gulp.task('bundle', function () {
   return browserify('src/index.js')
     .transform('babelify', {presets: ['es2015', 'react'], 'plugins': ['transform-object-rest-spread'], 'compact': false})
     .bundle()
+    .on('error', function (err) {
+      console.error(err.message)
+      console.error(err.codeFrame)
+    })
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('static/'))
 })
