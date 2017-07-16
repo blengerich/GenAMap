715487f98bfd380c16ce0c06b1ec3ef8e6412d43
Wenting Ye <Wenting_Ye@Outlook.com>
2017-07-16 15:21:21 -0400

    add "-e" flag && put command into backend

--- Documentation/Installation/install_genamap.sh
+++ Documentation/Installation/install_genamap.sh
@@ -1,18 +1,20 @@
+set -e
+
 # Install and start Mongo DB
 if [ ! -d ./mongodbpath ]; then
   mkdir mongodbpath
 fi
-sudo docker run -v "$(pwd)/mongodbpath":/data -p 27017:27017 --name mongo -d mongo mongod --smallfiles
+docker run -v "$(pwd)/mongodbpath":/data -p 27017:27017 --name mongo -d mongo mongod --smallfiles
 
 # Install and start Postgres DB
 if [ ! -d ./postgresdbpath ]; then
   mkdir postgresdbpath
 fi
 
-sudo docker run --name postgres -p 5432:5432 \
+docker run --name postgres -p 5432:5432 \
   -v "$(pwd)/postgresdbpath":/var/lib/postgresql/data \
   -e POSTGRES_PASSWORD='' \
   -d postgres
 
 # Install the main GenAMap Server
-sudo docker pull haohanwang/genamap_server
+docker pull haohanwang/genamap_server
