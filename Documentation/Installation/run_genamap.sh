8fba31cb32531dc66e0d9620b652348c5725fcbf
Jeremy Ong <tjong1998@gmail.com>
2017-07-29 21:02:41 -0400

    installation script checks for existing containers now and starts those if it can instead of creating new containers. it also now pulls the latest genamap image every 24 hours

--- Documentation/Installation/run_genamap.sh
+++ Documentation/Installation/run_genamap.sh
@@ -1,6 +1,31 @@
+#!/bin/bash
 set -e
 
-# Install Mongo DB and PostgreSQL
+function hr {
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
+# Download the latest Genamap Server image
+try_pull_every_n_secs=$((24*3600))	# Only try pulling the Docker once/day
+
+last_time=0
+if [ -e "last_docker_pull.start" ]
+then
+    last_time=$(cat last_docker_pull.start)
+fi
+cur_time=$(date +%s)
+let diff_time=$(($cur_time - $last_time))
+if [ "$diff_time" -ge "$try_pull_every_n_secs" ]
+then
+    hr
+    echo -e "Download the latest GenAMap server..."
+    hr
+    docker login || { echo 'docker login failed'; exit 1; }
+    docker pull haohanwang/genamap_server || { echo 'docker pull failed'; exit 1; }
+    date +%s > last_docker_pull.start
+fi
+
+
 if [ ! -d ./mongodbpath ]; then
   mkdir mongodbpath
 fi
@@ -8,18 +33,43 @@ if [ ! -d ./postgresdbpath ]; then
   mkdir postgresdbpath
 fi
 
-echo "It will take a while to download the database in the first time..."
+# Run Mongo DB Container
+if ! docker ps --format "{{.Names}}"| grep -q mongo; then
+    if ! docker ps -a --format "{{.Names}}"| grep -q mongo; then
+        { docker run -v "$(pwd)/mongodbpath":/data -p 27017:27017 --name mongo -d mongo mongod --smallfiles || { docker stop mongo; docker rm mongo; docker run -v "$(pwd)/mongodbpath":/data -p 27017:27017 --name mongo -d mongo mongod --smallfiles;} }  1> /dev/null
+    else
+        docker start mongo 1>/dev/null
+    fi
+    hr
+    echo "Mongo DB container has been successfully launched!"
+    hr
+fi
 
-{ docker run -v "$(pwd)/mongodbpath":/data -p 27017:27017 --name mongo -d mongo mongod --smallfiles || { docker stop mongo; docker rm mongo; docker run -v "$(pwd)/mongodbpath":/data -p 27017:27017 --name mongo -d mongo mongod --smallfiles;} }  &> /dev/null
-echo "mongo DB has been successfully launched!"
+# Run PostgresSQL container
 
-{ docker run --name postgres -p 5432:5432  -v "$(pwd)/postgresdbpath":/var/lib/postgresql/data -e POSTGRES_PASSWORD='!!GeNaMaPnew00' -e POSTGRES_USER='postgres' -d postgres || { docker stop postgres; docker rm postgres; docker run --name postgres -p 5432:5432  -v "$(pwd)/postgresdbpath":/var/lib/postgresql/data -e POSTGRES_PASSWORD='!!GeNaMaPnew00' -e POSTGRES_USER='postgres' -d postgres;} } &> /dev/null
-echo "PostgreSQL has been successfully launched!"
+if ! docker ps --format "{{.Names}}"| grep -q postgres; then
+    if ! docker ps -a --format "{{.Names}}"| grep -q postgres; then
+        { docker run --name postgres -p 5432:5432  -v "$(pwd)/postgresdbpath":/var/lib/postgresql/data -e POSTGRES_PASSWORD='!!GeNaMaPnew00' -e POSTGRES_USER='postgres' -d postgres || { docker stop postgres; docker rm postgres; docker run --name postgres -p 5432:5432  -v "$(pwd)/postgresdbpath":/var/lib/postgresql/data -e POSTGRES_PASSWORD='!!GeNaMaPnew00' -e POSTGRES_USER='postgres' -d postgres;} } 1> /dev/null
+    else
+        docker start postgres 1>/dev/null
+    fi
+    hr
+    echo "PostgreSQL container has been successfully launched!"
+    hr
+fi
 
-# Run the latest GenAMap server
-printf "\nDownload the latest GenAMap server...\n"
-docker pull haohanwang/genamap_server
-
-printf "\nStarting the server\n"
-docker run -d -p 49160:3000 --link mongo:mongo --link postgres:postgres haohanwang/genamap_server
-printf "\nServer is running in the background successfully..."
+# Run the GenAMap server
+if ! docker ps | grep -q haohanwang/genamap_server; then
+    if ! docker ps -a --format "{{.Image}}"| grep -q haohanwang/genamap_server; then
+        docker run -d -p 49160:3000 --link mongo:mongo --link postgres:postgres haohanwang/genamap_server 1> /dev/null
+    else
+        line=$(docker ps -a --format "{{.Image}}" | grep -n haohanwang/genamap_server | cut -d':' -f1)
+        docker start $(docker ps -a --format "{{.Names}}" | sed -n ${line}p)
+    fi
+    hr
+    echo "GenAMap Server container has been successfully launched!"
+    hr
+fi
+hr
+echo "Server is running in the background successfully on port 49160..."
+hr
