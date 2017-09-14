a7ed152f5f2091e2fee936f704ee9eb7ed196345
Wenting Ye <Wenting_Ye@Outlook.com>
2017-09-14 17:17:07 -0400

    update server shell

--- Documentation/Installation/run_genamap.sh
+++ Documentation/Installation/run_genamap.sh
@@ -69,7 +69,7 @@ fi
 g_name="genamap_production_server"
 if ! docker ps --format "{{.Names}}"| grep -q ${g_name}; then
     if ! docker ps -a --format "{{.Names}}"| grep -q ${g_name}; then
-        docker run -d -p 80:3000 --name ${g_name} --link ${m_name}:mongo --link ${p_name}:postgres haohanwang/genamap_server \
+        docker run -d -p 80:3000 --name ${g_name} --link ${m_name}:mongo --link ${p_name}:postgres -v $1:/usr/src/genamap_data -v $2:/usr/src/genamap_config haohanwang/genamap_server \
             || { echo "starting genamap failed" >&2; exit 1; }
     else
         docker start ${g_name} \
